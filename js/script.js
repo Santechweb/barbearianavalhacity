c66ac67f2cd3eb2bae04da46faf4d04c18b57e5d
Santechweb <gnsarq@gmail.com>
2026-04-28 07:07:36 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// JavaScript simples para melhorar a experiência do usuário.
+// Aqui você pode adicionar menus, animações ou integração futura.
+console.log('Site carregado com botão flutuante do WhatsApp em todas as páginas.');
